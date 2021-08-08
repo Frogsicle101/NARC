2a6e47acd6ca170d02519bb65b67cf4deb70b21a
fma107 <fma107@uclive.ac.nz>
2021-08-09 10:53:48 +1200

    Edited testfile so it compiles :)

--- src/test/java/seng202/group6/CodyTest.java
+++ src/test/java/seng202/group6/CodyTest.java
@@ -1,14 +1,13 @@
 package seng202.group6;
 
-import org.junit.Test;
+import junit.framework.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.*;
 
 public class CodyTest {
 
     Cody newTest = new Cody();
-
-    @Test
+    
     public void testMain() {
         assertEquals(newTest.printString(), "Wow it worked");
     }
