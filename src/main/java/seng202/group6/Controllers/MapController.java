1d3eecc6667353f9716635d8b09b1fb486da0ebe
Billy Sandri <bsa57@uclive.ac.nz>
2021-08-29 19:40:10 +1200

    Added variable zoom to static maps and message if textField empty.

--- src/main/java/seng202/group6/Controllers/MapController.java
+++ src/main/java/seng202/group6/Controllers/MapController.java
@@ -1,6 +1,5 @@
 package seng202.group6.Controllers;
 
-import com.google.maps.ImageResult;
 import com.google.maps.model.LatLng;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,12 +8,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import seng202.group6.Services.MapService;
-import javafx.scene.web.WebEngine;
 
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import static seng202.group6.Services.MapService.*;
@@ -42,6 +36,9 @@ public class MapController extends MasterController {
     @FXML
     private TextField addressField;
 
+    @FXML
+    private Text noAddressText;
+
     public void clickHome(ActionEvent event) throws IOException {
         changeToHomeScreen(event);
     }
@@ -54,38 +51,22 @@ public class MapController extends MasterController {
         changeToImportScreen(event);
     }
 
-    public void clickViewMap(ActionEvent event) throws IOException {
-        String address = addressField.getText().toString();
-
-        /*String testImage = "https://maps.googleapis.com/maps/api/staticmap?" +
-                "center=" + coord +
-                "&zoom=10" +
-                "&size=600x300" +
-                "&maptype=roadmap" +
-                "&key=AIzaSyBZgxE6A5nvnM7aYqg49wDdK_SPKXqdLiE";
-        Image image = getImage(testImage);
-        mapImage.setImage(image);*/
-/*
-        LatLng coord = null;
-        try {
-            coord = geoCodeAddress();
-            ImageResult imageResult = getImage(coord);
-            System.out.println(imageResult);
-            BufferedImage image = null;
-            ImageIO.write(image, "png", imageResult.imageData);
-            mapImage.setImage(image);
-        } catch(Exception e) {
-            System.out.println("Error " + e);
-        }*/
-        LatLng coord = null;
-        try {
-            //coord = geoCodeAddress(address);
-            Image image = getImage(address);
-            mapImage.setImage(image);
-        } catch(Exception e) {
-            System.out.println("Error " + e);
+    public void clickViewMap(ActionEvent event) {
+        String address = addressField.getText();
+        System.out.println(address.isEmpty());
+        if (!address.isEmpty()) {
+            noAddressText.setVisible(false);
+            LatLng coord = null;
+            try {
+                //coord = geoCodeAddress(address);
+                Image image = getStaticMap(address);
+                mapImage.setImage(image);
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+        } else {
+            noAddressText.setVisible(true);
         }
-
     }
 
 }
--- src/main/java/seng202/group6/Services/MapService.java
+++ src/main/java/seng202/group6/Services/MapService.java
@@ -4,38 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.*;
 import com.google.maps.errors.ApiException;
-import com.google.maps.model.GeocodingResult;
-import com.google.maps.model.LatLng;
-import com.google.maps.model.Size;
+import com.google.maps.model.*;
 import javafx.scene.image.Image;
 
 import javax.imageio.ImageIO;
-import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.URL;
 import javafx.embed.swing.SwingFXUtils;
 
 
 
 public class MapService {
     private static final String apiKey = "AIzaSyBZgxE6A5nvnM7aYqg49wDdK_SPKXqdLiE";
-
-    public static void saveImage(String imageUrl, String destinationFile) throws IOException {
-        URL url = new URL(imageUrl);
-        InputStream is = url.openStream();
-        OutputStream os = new FileOutputStream(destinationFile);
-
-        byte[] b = new byte[2048];
-        int length;
-
-        while ((length = is.read(b)) != -1) {
-            os.write(b, 0, length);
-        }
-
-        is.close();
-        os.close();
-    }
 
     public static LatLng geoCodeAddress(String address) throws IOException, InterruptedException, ApiException {
         GeoApiContext context = new GeoApiContext.Builder()
@@ -52,28 +32,87 @@ public class MapService {
         return results[0].geometry.location;
     }
 
+    public static int highestLevelZoom(String address) throws IOException, InterruptedException, ApiException {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+        GeocodingResult[] results =  GeocodingApi.geocode(context,
+                address).await();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+        System.out.println(gson.toJson(results[0].addressComponents));
+
+// Invoke .shutdown() after your application is done making requests
+        context.shutdown();
+
+        AddressComponent[] addressComponents = results[0].addressComponents;
+        AddressComponentType[] addressComponentTypes = addressComponents[0].types;
+
+        int zoomLevel = 10;
+        boolean isFoundZoom = false;
+        int i = 0;
+        while (!isFoundZoom) {
+            switch (addressComponents[i].types[0].name()) {
+                case "STREET_NUMBER":
+                    ;
+                case "ROUTE":
+                    zoomLevel = 17;
+                    isFoundZoom = true;
+                    break;
+                case "POLITICAL":
+                    for (AddressComponentType type : addressComponentTypes) {
+                        if (type.name() == "SUBLOCALITY"){
+                            zoomLevel = 14;
+                            isFoundZoom = true;
+                        }
+                    }
+                    break;
+                case "LOCALITY":
+                    zoomLevel = 10;
+                    isFoundZoom = true;
+                    break;
+                case "ADMINISTRATIVE_AREA_LEVEL_1":
+                    zoomLevel = 8;
+                    isFoundZoom = true;
+                    break;
+                case "COUNTRY":
+                    zoomLevel = 5;
+                    isFoundZoom = true;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + addressComponents[i].types[0].name());
+            }
+            i++;
+        }
+
+        return zoomLevel;
+    }
+
+/*
     public static Image getImage(LatLng centre) throws IOException, InterruptedException, ApiException {
         GeoApiContext context = new GeoApiContext.Builder()
                 .apiKey(apiKey)
                 .build();
         Size size = new Size(600, 300);
+
         ImageResult request = StaticMapsApi.newRequest(context, size).center(centre).zoom(10).await();
         Image image = arrayToImage(request.imageData);
         return image;
-    }
+    }*/
 
-    public static Image getImage(String centre) throws IOException, InterruptedException, ApiException {
+    public static Image getStaticMap(String centre) throws IOException, InterruptedException, ApiException {
         GeoApiContext context = new GeoApiContext.Builder()
                 .apiKey(apiKey)
                 .build();
-        Size size = new Size(600, 300);
-        ImageResult request = StaticMapsApi.newRequest(context, size).center(centre).zoom(10).await();
+        Size size = new Size(700, 350);
+        int zoom = highestLevelZoom(centre);
+        ImageResult request = StaticMapsApi.newRequest(context, size).center(centre).zoom(zoom).maptype(StaticMapsRequest.StaticMapType.roadmap).await();
         Image image = arrayToImage(request.imageData);
+        context.shutdown();
         return image;
     }
 
-    public static Image arrayToImage(byte[] byte_array) throws IOException {
+    public static Image arrayToImage(byte[] byte_array) {
         Image image = null;
         try {
             ByteArrayInputStream input_stream = new ByteArrayInputStream(byte_array);
@@ -87,14 +126,5 @@ public class MapService {
         return image;
     }
 
-    /*public static void main(String[] args) {
-        LatLng coord = null;
-        try {
-            coord = geoCodeAddress();
-            Image image = getImage(coord);
-            System.out.println(image);
-        } catch(Exception e) {
-            System.out.println("Error " + e);
-        }
-    }*/
+
 }
