099b3081ff8bdf51e70588c26fc63723f2c96f8b
wil-r-johnston <wjo34@canterbury.ac.nz>
2021-09-05 13:40:48 +1200

    Added rank data by area methods

--- src/main/java/seng202/group6/Services/Rank.java
+++ src/main/java/seng202/group6/Services/Rank.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 public class Rank {
     /**
-     * Ranks data by passed in String parameter
+     * Ranks data by passed in String parameter crime type
      * @param crimes The input to count types
      * @param type   A string of primary type that will be counted
      * @return The number of certain crime type
@@ -38,6 +38,43 @@ public class Rank {
         ArrayList<String> output = new ArrayList<>();
         for (Crime crime : crimes) {
             initialList.add(crime.getPrimaryDescription());
+        }
+        initialList.sort(Comparator.comparing(i -> Collections.frequency(initialList, i)).reversed());
+
+        for (String element : initialList) {
+            if (!output.contains(element)) {
+                output.add(element);
+            }
+        }
+        return output;
+    }
+
+    /**
+     * Ranks data by passed in String parameter area
+     * @param crimes The input to count types
+     * @param area A string of the block area that will be counted
+     * @return The number of certain crime in an area
+     */
+    public static int frequencyOfArea(ArrayList<Crime> crimes, String area) {
+        int freqCount = 0;
+        for (Crime crime : crimes) {
+            if (crime.getBlock().substring(0,4).equals(area.substring(0,4))) {
+                freqCount++;
+            }
+        }
+        return freqCount;
+    }
+
+    /**
+     * Sorts data by frequency of crime area
+     * @param crimes The input to be ranked
+     * @return Array list of type String in decreasing order of crime area
+     */
+    public static ArrayList<String> rankedAreaList(ArrayList<Crime> crimes) {
+        ArrayList<String> initialList = new ArrayList<>();
+        ArrayList<String> output = new ArrayList<>();
+        for (Crime crime : crimes) {
+            initialList.add(crime.getBlock().substring(0,4));
         }
         initialList.sort(Comparator.comparing(i -> Collections.frequency(initialList, i)).reversed());
 
