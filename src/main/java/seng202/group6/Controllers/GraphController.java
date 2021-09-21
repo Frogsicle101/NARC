5cc57975f536792216c222eaadc3cfe99167f0a1
James <jcl165@uclive.ac.nz>
2021-09-21 15:52:26 +1200

    Removed the unesscary recreatoin of graphs when the data has not changed.

--- src/main/java/seng202/group6/Controllers/GraphController.java
+++ src/main/java/seng202/group6/Controllers/GraphController.java
@@ -2,14 +2,19 @@ package seng202.group6.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import seng202.group6.Models.CrimeFrequency;
 import seng202.group6.Models.TimeFrequency;
+import seng202.group6.Services.SQLiteDatabase;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.ResourceBundle;
@@ -40,12 +45,29 @@ public class GraphController extends MasterController implements Initializable {
     private Button applyChartButton;
 
     @FXML
-    private LineChart<String, Number> lineChart;
+    private LineChart<Number, Number> lineChart;
 
-    private ArrayList<TimeFrequency> data;
+    private ArrayList<TimeFrequency> data = new ArrayList<TimeFrequency>();
+
+    @FXML
+    public NumberAxis xAxis;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lineChart.getData().clear();
+        xAxis.setUpperBound(23);
+        xAxis.setLowerBound(0);
+        xAxis.forceZeroInRangeProperty();
+        xAxis.setTickUnit(1);
+        xAxis.setTickLabelGap(1);
+
+
+
+//        try {
+//            data = rankedTimeList(SQLiteDatabase.convertResultSet(SQLiteDatabase.selectAllFromTable("Crimes")));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
         mapButton.setFocusTraversable(false);
         dataButton.setFocusTraversable(false);
@@ -98,34 +120,45 @@ public class GraphController extends MasterController implements Initializable {
      * @throws IOException
      */
     public void clickApplyChart() throws IOException {
-        lineChart.getData().clear();
-        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
-        if (filteredCrimeData.size() != 0) {
-            data = rankedTimeList(filteredCrimeData); //DO we want this from filtered data or
-        } else {
-            data = rankedTimeList(crimeData);
-        }
-        data.sort(Comparator.comparing(TimeFrequency::getHourOfTheDay));
-        for (int i=0; i < 24; i++) {
-            boolean found = false;
-            int index = 0;
-            for (TimeFrequency time : data) {
-                if (time.getHourOfTheDay() == i) {
-                    found = true;
-                    index = data.indexOf(time);
+       if ((data.size() == 0 && crimeData.size() == 0) || (data.size() != rankedTimeList(filteredCrimeData).size() && filteredCrimeData.size() != 0) || (
+               data.size() != rankedTimeList(crimeData).size() && crimeData.size() != 0 && filteredCrimeData.size() == 0)) {
+            XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+            lineChart.getData().clear();
+            if (filteredCrimeData.size() != 0) {
+                data = rankedTimeList(filteredCrimeData); //DO we want this from filtered data or
+            } else {
+                if (crimeData.size() != 0) {
+                    data = rankedTimeList(crimeData);
+                } else {
+                    try {
+                        data = rankedTimeList(SQLiteDatabase.convertResultSet(SQLiteDatabase.selectAllFromTable("Crimes")));
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            if (found) {
-                series.getData().add(new XYChart.Data<String, Number>(valueOf(data.get(index).getHourOfTheDay()), data.get(index).getFrequency()));
-            } else {
-                series.getData().add(new XYChart.Data<String, Number>(valueOf(i), 0));
+            data.sort(Comparator.comparing(TimeFrequency::getHourOfTheDay));
+            for (int i = 0; i < 24; i++) {
+                boolean found = false;
+                int index = 0;
+                for (TimeFrequency time : data) {
+                    if (time.getHourOfTheDay() == i) {
+                        found = true;
+                        index = data.indexOf(time);
+                    }
+                }
+                if (found) {
+                    series.getData().add(new XYChart.Data<Number, Number>(data.get(index).getHourOfTheDay(), data.get(index).getFrequency()));
+                } else {
+                    series.getData().add(new XYChart.Data<Number, Number>(i, 0));
+                }
+
             }
 
+            series.setName("Crime Frequency Over Day");
+            lineChart.getData().add(series);
+
         }
-
-        series.setName("Crime Frequency Over Day");
-        lineChart.getData().add(series);
-
     }
 
 }
