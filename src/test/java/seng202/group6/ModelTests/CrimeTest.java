e6c3650aef78962f48572067761df4b36c8d7f68
fma107 <fma107@uclive.ac.nz>
2021-09-25 17:44:04 +1200

    Edited CrimeTest for formatting and naming conventions

--- src/test/java/seng202/group6/ModelTests/CrimeTest.java
+++ src/test/java/seng202/group6/ModelTests/CrimeTest.java
@@ -9,15 +9,14 @@ import seng202.group6.Models.Crime;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CrimeTest {
 
 
     @Test
-    public void equalsFalseTest() {
+    public void differentCrimesTest() {
         LocalDateTime time = LocalDateTime.now();
         Crime crime1 = new Crime("jfc123",
                 time,
@@ -25,14 +24,14 @@ public class CrimeTest {
                 "a1",
                 "crime",
                 "crimecrime",
-        true,
-        true,
-        2,
-        8,
-        "fbi",
-        "fbi",
-        23.2,
-        8.9);
+                true,
+                true,
+                2,
+                8,
+                "fbi",
+                "fbi",
+                23.2,
+                8.9);
         Crime crime2 = new Crime("jfc123",
                 time,
                 "023XXXX",
@@ -48,12 +47,12 @@ public class CrimeTest {
                 23.2,
                 8.9);
 
-        assertTrue(!(crime1.equals(crime2)));
+        assertFalse(crime1.equals(crime2));
 
     }
 
     @Test
-    public void assertEqualsTrue() {
+    public void sameCrimesTest() {
         LocalDateTime time = LocalDateTime.now();
         Crime crime1 = new Crime("jfc123",
                 time,
@@ -88,7 +87,7 @@ public class CrimeTest {
 
     @Test
     public void getReadableDateTest() {
-        LocalDateTime time = LocalDateTime.of(2020, 12, 31, 23, 59, 00);
+        LocalDateTime time = LocalDateTime.of(2020, 12, 31, 23, 59, 0);
         Crime crime = new Crime();
         crime.setDate(time);
         assertEquals("31-12-2020 23:59:00", crime.getReadableDate());
