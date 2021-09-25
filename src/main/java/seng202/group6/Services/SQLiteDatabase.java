7d75668d2e76e10f98c349a455ebd343ce08ed37
fma107 <fma107@uclive.ac.nz>
2021-09-25 15:13:39 +1200

    Added Javadoc to SQLiteDatabase

--- src/main/java/seng202/group6/Services/SQLiteDatabase.java
+++ src/main/java/seng202/group6/Services/SQLiteDatabase.java
@@ -4,16 +4,25 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-import com.google.maps.model.LatLng;
-import seng202.group6.Controllers.MasterController;
+
 import seng202.group6.Models.Crime;
 
+/**
+ * This class contains code for interacting with the database, including creating the database,
+ * creating and dropping tables, and inserting, editing, and deleting crime objects from the database.
+ *
+ * Importantly, for efficiency reasons the database is set to not autocommit changes to the database.
+ * Thus, after making any changes to the database endTransaction() should be called to commit those changes.
+ */
+
 public class SQLiteDatabase {
+
     private static final String jdbcUrl = "jdbc:sqlite:crimes.db";  //Constant that stores path to database file
     private static Connection connection;   //Stores database connection. Initialised by connectToDatabase()
 
     /**
      * Creates a connection to the database, and creates it if it doesn't exist
+     * @param jdbcUrl The path to the database file
      */
     public static void connectToDatabase(String jdbcUrl) throws SQLException {
         connection = DriverManager.getConnection(jdbcUrl);
@@ -21,13 +30,6 @@ public class SQLiteDatabase {
 
     }
 
-    public static String getJdbcUrl() {
-        return jdbcUrl;
-    }
-
-    public static Connection getConnection() {
-        return connection;
-    }
 
     /**
      * Checks whether a table exists in the database, and creates it if it doesn't exist
@@ -68,6 +70,11 @@ public class SQLiteDatabase {
 
     }
 
+    /**
+     * Updates a crime object in a particular table
+     * @param tableName The name of the table
+     * @param crime The new object
+     */
     public static void updateInTable(String tableName, Crime crime) throws SQLException{
         String sql = "UPDATE " + tableName + " " +
                 "SET " +
@@ -92,6 +99,12 @@ public class SQLiteDatabase {
 
     }
 
+    /**
+     * Deletes a crime from a particular table
+     * More precisely, this method deletes the record that has the same crime_id as the given Crime
+     * @param tableName The name of the table to delete from
+     * @param crime The crime to delete
+     */
     public static void deleteFromTable(String tableName, Crime crime) throws SQLException {
         String sql = "DELETE FROM " + tableName +
                 " WHERE case_id = '" + crime.getCaseNumber() + "';";
@@ -102,6 +115,10 @@ public class SQLiteDatabase {
         endTransaction();
     }
 
+    /**
+     * Gets the names of all user created tables in the database as a ResultSet
+     * @return The ResultSet, which should be closed after usage
+     */
     public static ResultSet getTableNames() throws SQLException {
 
         String sql = "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite%';";
@@ -112,7 +129,6 @@ public class SQLiteDatabase {
 
     /**
      * Ends an SQL transaction and commits the results to file. Call after every change to the database
-     * @throws SQLException
      */
     public static void endTransaction() throws SQLException {
         connection.commit();
@@ -129,6 +145,13 @@ public class SQLiteDatabase {
         return statement.executeQuery(sql);
     }
 
+    /**
+     * Converts the given ResultSet to an arrayList of type Crimes.
+     * This method also closes the ResultSet.
+     * @param data The ResultSet
+     * @return An arraylist of Crimes
+     * @throws SQLException If called on a ResultSet that does not contain crimes
+     */
     public static ArrayList<Crime> convertResultSet(ResultSet data) throws SQLException {
         ArrayList<Crime> out = new ArrayList<>();
 
@@ -155,23 +178,23 @@ public class SQLiteDatabase {
         return out;
     }
 
-    /*public static ResultSet selectLocationsFromTable(LatLng location) throws SQLException {
-        Double latRight = location.lat - 0.0036;
-        Double latLeft = location.lat + 0.0031;
-        Double lngUp = location.lng - 0.0062;
-        Double lngDown = location.lng + 0.0062;
-        String sql = "SELECT * FROM crimes WHERE (latitude BETWEEN "+latRight+" AND "
-                +latLeft+") AND (longitude BETWEEN "+lngUp +" AND "+lngDown+")";
-        Statement statement = connection.createStatement();
-        return statement.executeQuery(sql);
-    }*/
-
+    /**
+     * Executes the given SQL query on the database
+     * @param query The Query
+     * @return A ResultSet from the database
+     * @throws SQLException If the query is erroneous
+     */
     public static ResultSet executeQuery(String query) throws SQLException {
 
         Statement statement = connection.createStatement();
         return statement.executeQuery(query);
     }
 
+    /**
+     * Drops a table
+     * @param tableName The name of the table to drop
+     * @throws SQLException If the table does not exist
+     */
     public static void dropTable(String tableName) throws SQLException {
 
         String sql = "DROP TABLE " + tableName + ";";
@@ -180,5 +203,13 @@ public class SQLiteDatabase {
         statement.executeUpdate(sql);
 
         endTransaction();
+    }
+
+    public static String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public static Connection getConnection() {
+        return connection;
     }
 }
