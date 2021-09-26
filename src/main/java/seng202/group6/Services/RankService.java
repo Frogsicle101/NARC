a2c497f8bef6a0adfb4d3365133737bd2e4f9b25
fma107 <fma107@uclive.ac.nz>
2021-09-26 13:12:32 +1300

    Update javadoc for rank service

--- src/main/java/seng202/group6/Services/RankService.java
+++ src/main/java/seng202/group6/Services/RankService.java
@@ -9,14 +9,21 @@ import java.util.HashMap;
 import java.util.function.Function;
 
 /**
- * class used for ranking of crimes in terms of Area, Type of crime and Time
+ * Class used for ranking of crimes in terms of area (block number), type of crime,
+ * and time. You can rank by different measures of time by passing a TimeType.
  */
 
 
 public class RankService {
 
 
-
+    /**
+     * Rank crimes using the given function
+     * @param crimes An ArrayList of Crimes to rank
+     * @param function A function that takes a crime and returns a property of that crime
+     *                 For example (crime -> crime.getPrimaryDescription())
+     * @return An ArrayList of Frequency objects.
+     */
     private static ArrayList<Frequency> rankedList(ArrayList<Crime> crimes, Function<Crime, String> function) {
         HashMap<String, Integer> frequencies = new HashMap<>();
         for (Crime crime : crimes) {
@@ -35,9 +42,9 @@ public class RankService {
     }
 
     /**
-     * Using the CrimeFrequency class this method sorts an array of crimes into an ordered list of Crime Frequencies and returns it
+     * Sorts an array of crimes into an ordered list of frequencies based on their primary description
      * @param crimes An array list of Crime objects
-     * @return A sorted array list of CrimeFrequencies
+     * @return A sorted array list of Frequencies
      */
     public static ArrayList<Frequency> rankedTypeList(ArrayList<Crime> crimes) {
         return rankedList(crimes, Crime::getPrimaryDescription);
@@ -47,18 +54,21 @@ public class RankService {
 
 
     /**
-     * Using the AreaFrequency class this method sorts an array of crimes into an ordered list of AreaFrequency objects and returns it
+     * Sorts an ArrayList of crimes into an ordered list of frequencies based on their block number (the first
+     * three numbers of the block)
      * @param crimes An array list of Crime objects
-     * @return A sorted array list of AreaFrequency
+     * @return A sorted array list of Frequencies
      */
     public static ArrayList<Frequency> rankedAreaList(ArrayList<Crime> crimes) {
         return rankedList(crimes, crime -> crime.getBlock().substring(0, 3));
     }
 
     /**
-     * Using the HourOfDayFrequency class this method sorts an array of crimes into an ordered list of HourOfDayFrequency objects and returns it
+     * Sorts an ArrayList of crimes into an ordered list of frequencies based on their current time.
+     * Different measures of time can be selected by changing timeType
      * @param crimes An array list of Crime objects
-     * @return A sorted array list of HourOfDayFrequency
+     * @param timeType A TimeType enum representing what type of time filtering is required.
+     * @return A sorted array list of Frequencies
      */
     public static ArrayList<Frequency> rankedTimeList(ArrayList<Crime> crimes, TimeType timeType) {
 
