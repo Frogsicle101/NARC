283b1cfa897cb48c1afaf7f30c7e8fe890371f94
Frederik Markwell <fma107@uclive.ac.nz>
2021-10-11 15:55:55 +1300

    Added Javadoc for ApiKey

--- src/main/java/seng202/group6/DynamicMapSRC/ApiKey.java
+++ src/main/java/seng202/group6/DynamicMapSRC/ApiKey.java
@@ -1,5 +1,8 @@
 package seng202.group6.DynamicMapSRC;
 
+/**
+ * Holds the API key to connect with Google Maps.
+ */
 public class ApiKey {
     private static final String apiKey = "AIzaSyBZgxE6A5nvnM7aYqg49wDdK_SPKXqdLiE";
 
