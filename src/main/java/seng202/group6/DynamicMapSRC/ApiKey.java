317bed0650254a16d7980903a0df7b32249b7b6e
Billy Sandri <bsa57@uclive.ac.nz>
2021-10-24 17:35:23 +1300

    Deleted api key from google cloud services

--- src/main/java/seng202/group6/DynamicMapSRC/ApiKey.java
+++ src/main/java/seng202/group6/DynamicMapSRC/ApiKey.java
@@ -4,7 +4,7 @@ package seng202.group6.DynamicMapSRC;
  * Holds the API key to connect with Google Maps.
  */
 public class ApiKey {
-    private static final String apiKey = "AIzaSyBZgxE6A5nvnM7aYqg49wDdK_SPKXqdLiE";
+    private static final String apiKey = "YOUR_KEY";
 
     public static String getApiKey() {
         return apiKey;
--- src/main/resources/seng202/group6/HTML/JavascriptConfig.js
+++ src/main/resources/seng202/group6/HTML/JavascriptConfig.js
@@ -1,6 +1,6 @@
 function getApiKey() {
   var script = document.createElement("script");
-  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBZgxE6A5nvnM7aYqg49wDdK_SPKXqdLiE&libraries=places&callback=initMap&v=weekly";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=YOU_KEY&libraries=places&callback=initMap&v=weekly";
   script.async;
   document.body.appendChild(script);
 }
